86b656d849f2a561bfe75b3315985a5c6898ff44
José de León Suarez <jleon@aginc.local>
2018-03-26 10:51:26 -0400

    agregando clase catalogo contante

---
+++ DiscoveryFeaturePortal/src/main/java/com/discovery/feature/portal/mvc/constante/ParametrosCatalogoConstante.java
@@ -0,0 +1,11 @@
+package com.discovery.feature.portal.mvc.constante;
+
+public class ParametrosCatalogoConstante {
+
+	public static interface FeatureType {
+		int CONST_PANTALLA = 1;
+		int CONST_FLUJOS = 2;
+		int CONST_MIXTO = 3;
+	}
+
+}
